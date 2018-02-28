735c04a9630a14d2c4e70abeda0388d3aac15db9
zrolston <zrolston@ufl.edu>
2018-02-28 14:10:38 -0500

    junit includes

--- src/SparseInterfaceTest.java
+++ src/SparseInterfaceTest.java
@@ -1,6 +1,6 @@
 import org.junit.jupiter.api.Test;
-import static junit.framework.TestCase.assertEquals;
-import static junit.framework.TestCase.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 class SparseInterfaceTest {
